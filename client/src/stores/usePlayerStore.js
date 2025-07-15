3cfdb91116e80e176feab850251771cb3633e10e
lunoxdev <lunox.code@gmail.com>
2025-07-15 02:21:08 -0600

    Refactor usePlayerStore to integrate Zustand devtools for enhanced debugging

--- client/src/stores/usePlayerStore.js
+++ client/src/stores/usePlayerStore.js
@@ -1,8 +1,11 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
-export const usePlayerStore = create((set) => ({
-  name: "",
-  balance: 0,
-  setName: (name) => set({ name }),
-  setBalance: (newBalance) => set({ balance: newBalance }),
-}));
+export const usePlayerStore = create(
+  devtools((set) => ({
+    name: "",
+    balance: 0,
+    setName: (name) => set({ name }),
+    setBalance: (newBalance) => set({ balance: newBalance }),
+  }))
+);
