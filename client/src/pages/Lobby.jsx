4cb27cef42ba2eb6bbaee0c2bb575244a6642f0f
lunoxdev <lunox.code@gmail.com>
2025-07-18 18:40:35 -0600

    feat: start match redirection

--- client/src/pages/Lobby.jsx
+++ client/src/pages/Lobby.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import { usePlayerStore } from "../stores/usePlayerStore";
 import { useRoomsStore } from "../stores/useRoomsStore";
 import { useLobbySocket } from "../hooks/useLobbySocket";
+import { useNavigate } from "react-router-dom";
 import SignUp from "../components/SignUp";
 import socket from "../socket";
 
 const Lobby = () => {
+  const navigate = useNavigate();
   const { name, balance, registered, token, signOut } = usePlayerStore();
   const { myRoom, availableRooms, setRoomId, clearRoom } = useRoomsStore();
 
@@ -17,9 +19,9 @@ const Lobby = () => {
 
   const handleSignOut = () => {
     socket.emit("signOut", token); // Notify server to remove player
-    signOut(); // Zustand cleanup
-    clearRoom();
-    setRoomId(null);
+    signOut(); // ⚠️ CHECK THIS
+    clearRoom(); // ⚠️ CHECK THIS
+    setRoomId(null); // ⚠️ CHECK THIS
   };
 
   const handleCreateRoom = () => {
@@ -42,6 +44,10 @@ const Lobby = () => {
     setRoomId(roomIdToJoin);
   };
 
+  const handleStartMatch = () => {
+    navigate("/match");
+  };
+
   return (
     <div className="flex flex-col items-center justify-center">
       {registered ? (
@@ -52,53 +58,56 @@ const Lobby = () => {
               {name}
             </span>
           </h1>
-          <p className="text-lg mb-6">
+          <p className="text-lg mb-4">
             Your balance:{" "}
             <span className="bg-gradient-to-r from-amber-500 via-amber-400 to-amber-500 inline-block text-transparent bg-clip-text font-semibold">
               ${balance}
             </span>
           </p>
 
-          <p>Players Connected:</p>
-          <ul className="mb-2 bg-gradient-to-r from-emerald-500 via-emerald-400 to-emerald-500 inline-block text-transparent bg-clip-text">
+          <p className="font-bold text-lg">Players Connected:</p>
+          <ul className="mb-4 bg-gradient-to-r from-emerald-500 via-emerald-400 to-emerald-500 inline-block text-transparent bg-clip-text">
             {players.map((player, index) => (
               <li key={index}>{player.name}</li>
             ))}
           </ul>
 
+          <p className="font-bold text-lg">All Rooms:</p>
           {availableRooms.length > 0 && (
-            <>
-              <p>Available Rooms:</p>
-              <ul>
-                {availableRooms.map((room, index) => (
-                  <li key={index}>
+            <ul>
+              {availableRooms
+                .filter((room) => !room.players.some((p) => p.name === name))
+                .map((room, index) => (
+                  <li
+                    key={index}
+                    className="border border-white rounded-md px-6 py-2 my-2"
+                  >
                     Room: {room.roomId} — Host: {room.host.name}
                     <br />
-
                     {/* Display join button if the player is not the host and if there are not 2 players yet */}
                     {room.host.name !== name &&
                       !room.players.some((p) => p.name === name) && (
-                        <button onClick={() => handleJoin(room.roomId)}>
+                        <button
+                          onClick={() => handleJoin(room.roomId)}
+                          className="border border-white rounded-md px-6 py-1 mt-4"
+                        >
                           Join
                         </button>
                       )}
                   </li>
                 ))}
-              </ul>
-            </>
+            </ul>
           )}
 
           <br />
 
+          <p className="font-bold text-lg">My Room:</p>
           {roomId && (
             <>
-              <p>Players in Room:</p>
               <p>Room ID: {roomId}</p>
               <ul>
                 {roomPlayers.map((p, index) => (
-                  <li key={index}>
-                    {p.name} — ${p.balance}
-                  </li>
+                  <li key={index}>{p.name}</li>
                 ))}
               </ul>
             </>
@@ -106,7 +115,21 @@ const Lobby = () => {
 
           <br />
 
-          <button onClick={handleCreateRoom}>Create Room</button>
+          {roomId && roomPlayers.length === 2 ? (
+            <button
+              onClick={handleStartMatch}
+              className="bg-cyan-600 px-4 py-1 rounded hover:bg-cyan-700 transition mb-4"
+            >
+              Start Match
+            </button>
+          ) : (
+            <button
+              onClick={handleCreateRoom}
+              className="bg-lime-600 px-4 py-1 rounded hover:bg-lime-700 transition mb-4"
+            >
+              Create Room
+            </button>
+          )}
 
           <br />
 
