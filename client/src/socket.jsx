291709f4735e5f2ad4e77ea17e251259547d34e4
lunoxdev <lunox.code@gmail.com>
2025-07-28 21:00:58 -0600

    update socket url

--- client/src/socket.jsx
+++ client/src/socket.jsx
@@ -1,5 +1,6 @@
 // Main entry point for the socket connection
 // This file is used to connect to the server and handle socket events
 import { io } from "socket.io-client";
-const socket = io("http://localhost:3001");
+const socket = io(import.meta.env.VITE_SOCKET_URL);
+
 export default socket;
