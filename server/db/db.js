a6997b3de6aa4dae67946252065d1421ab3a2c40
lunoxdev <lunox.code@gmail.com>
2025-08-03 22:58:57 -0600

    remove port

--- server/db/db.js
+++ server/db/db.js
@@ -3,7 +3,6 @@ const { Pool } = pkg;
 
 const pool = new Pool({
   host: process.env.DB_HOST,
-  port: 5432,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
