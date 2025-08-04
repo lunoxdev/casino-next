2ac3e1baf4cc0c72172fd907ffcf52509ffd3c99
lunoxdev <lunox.code@gmail.com>
2025-08-03 23:07:40 -0600

    add debug code

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import { Server } from "socket.io";
 import cors from "cors";
 import manageSockets from "./socket/index.js";
 import playerRoutes from "./routes/player.js";
+import pool from "./db.js";
 
 const app = express();
 const server = http.createServer(app);
@@ -12,6 +13,11 @@ const io = new Server(server, { cors: { origin: "*" } });
 app.use(cors());
 app.use(express.json());
 app.use("/api/player", playerRoutes);
+
+pool
+  .query("SELECT NOW()")
+  .then((res) => console.log("✅ DB connected at:", res.rows[0].now))
+  .catch((err) => console.error("❌ DB connection error:", err));
 
 manageSockets(io);
 
