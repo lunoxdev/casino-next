cc2266bab6582e49d5565000bb433506d7a18d7f
lunoxdev <lunox.code@gmail.com>
2025-08-04 00:21:28 -0600

    update db

--- server/db/db.js
+++ server/db/db.js
@@ -2,11 +2,7 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.POSTGRES_HOST,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
-  port: 5432,
+  connectionString: process.env.POSTGRES_URL,
   ssl: { rejectUnauthorized: false },
 });
 
