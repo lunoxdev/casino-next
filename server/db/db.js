a7f55422e56847d7beb9785d4798ed5b498ac645
lunoxdev <lunox.code@gmail.com>
2025-08-04 10:48:05 -0600

    udpate db

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ import pg from "pg";
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
+  connectionString: process.env.POSTGRES_URL_NON_POOLING,
   ssl: { rejectUnauthorized: false },
 });
 
