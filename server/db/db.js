a1b06b546219384c82104bd12b848bbb11240cf9
lunoxdev <lunox.code@gmail.com>
2025-08-04 00:34:08 -0600

    udpate db

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,9 @@
-import pkg from "pg";
-const { Pool } = pkg;
+import pg from "pg";
+const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 export default pool;
