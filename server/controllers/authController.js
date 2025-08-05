15842d686e69595ec0715f4057331cb8d536a026
lunoxdev <lunox.code@gmail.com>
2025-08-04 21:04:43 -0600

    remove import dotenv

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,8 +1,6 @@
 import supabase from "../db/supabase.js";
 import { v4 as uuidv4 } from "uuid";
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-dotenv.config();
 
 const JWT_SECRET = process.env.SUPABASE_JWT_SECRET;
 
