3a0e8b97fba49b918758264fa157d0541448f350
lunoxdev <lunox.code@gmail.com>
2025-08-04 21:10:04 -0600

    update var name

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
 import manageSockets from "./socket/index.js";
-import playerRoutes from "./routes/playerAuth.js";
+import authRoutes from "./routes/playerAuth.js";
 
 const app = express();
 const server = http.createServer(app);
@@ -11,7 +11,7 @@ const io = new Server(server, { cors: { origin: "*" } });
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/playerAuth", playerRoutes);
+app.use("/api/playerAuth", authRoutes);
 
 manageSockets(io);
 
