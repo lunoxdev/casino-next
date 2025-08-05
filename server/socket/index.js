613ad497a612e6f4444018028e5ea22858cd21c9
lunoxdev <lunox.code@gmail.com>
2025-08-04 21:49:16 -0600

    improve console logs

--- server/socket/index.js
+++ server/socket/index.js
@@ -3,7 +3,7 @@ import matchSockets from "./match/matchSockets.js";
 
 export default function manejarSockets(io) {
   io.on("connection", (socket) => {
-    console.log(`🟢 Player with socket ID: ${socket.id} connected`);
+    console.log(`🟢 Player connected. Socket ID: ${socket.id}`);
 
     // Lobby socket handlers
     lobbySockets(socket, io);
@@ -13,29 +13,7 @@ export default function manejarSockets(io) {
 
     // Disconnect event
     socket.on("disconnect", (reason) => {
-      switch (reason) {
-        case "io server disconnect":
-          console.log(
-            "🔌 Disconnected by the server (possibly kicked or forced)."
-          );
-          break;
-        case "io client disconnect":
-          console.log("👋 Disconnected by the user (logged out or refreshed).");
-          break;
-        case "ping timeout":
-          console.log("⏱️ Connection lost — server did not respond in time.");
-          break;
-        case "transport close":
-          console.log("📡 Connection closed (network lost or switched).");
-          break;
-        case "transport error":
-          console.log(
-            "⚠️ Connection error (server crashed or network failure)."
-          );
-          break;
-        default:
-          console.log("Disconnected: ", reason);
-      }
+      console.log(`🔌 Player disconnected (${reason})`);
     });
   });
 }
