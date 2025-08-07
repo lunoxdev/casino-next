0965e1e7cbd346e24b2d26d0800e1d539e7f9def
lunoxdev <lunox.code@gmail.com>
2025-08-06 19:00:31 -0600

    add import veriyT

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { verifyToken } from "../middlewares/verifyToken.js";
 import { getProfile } from "../controllers/profileController.js";
 
 const router = express.Router();
