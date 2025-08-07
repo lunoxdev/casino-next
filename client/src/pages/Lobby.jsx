f24ec274511307f378abaa92bb6d69aee18acc1f
lunoxdev <lunox.code@gmail.com>
2025-08-07 14:16:14 -0600

    udpate lobby

--- client/src/pages/Lobby.jsx
+++ client/src/pages/Lobby.jsx
@@ -3,7 +3,7 @@ import AuthForm from "../components/auth/AuthForm";
 import Profile from "../components/lobby/Profile";
 
 const Lobby = () => {
-  const loggedIn = useAuthStore((state) => state.loggedIn);
+  const { loggedIn } = useAuthStore();
 
   return (
     <div className="flex flex-col items-center justify-center">
