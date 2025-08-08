1d7668c07e46d0cb72b95dde5d8a408e443dd65c
lunoxdev <lunox.code@gmail.com>
2025-08-07 21:17:54 -0600

    update name to players List

--- server/state/playersStore.js
+++ server/state/playersStore.js
@@ -1,15 +1,13 @@
-// Store for managing player data in the server
-// This file is used to keep track of players and their balances
-export const players = new Map();
+export const playersList = new Map();
 
 export const addPlayer = (uuid, playerData) => {
-  players.set(uuid, playerData);
+  playersList.set(uuid, playerData);
 };
 
 export const removePlayer = (uuid) => {
-  players.delete(uuid);
+  playersList.delete(uuid);
 };
 
 export const getPlayersList = () => {
-  return Array.from(players.values());
+  return Array.from(playersList.values());
 };
