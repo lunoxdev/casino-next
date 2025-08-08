be2ca2e19ff3bf965771d46ff4d8e6e5f1eac755
lunoxdev <lunox.code@gmail.com>
2025-08-07 22:39:22 -0600

    add initial state in rooms store

--- client/src/stores/useRoomsStore.jsx
+++ client/src/stores/useRoomsStore.jsx
@@ -1,35 +1,44 @@
 import { create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
 
+const initialRoomsState = {
+  myRoom: {
+    roomId: null,
+    gameName: null,
+    roomPlayers: [],
+  },
+
+  availableRooms: [],
+};
+
 export const useRoomsStore = create(
   devtools(
-    persist((set) => ({
-      myRoom: {
-        roomId: null,
-        gameName: null,
-        roomPlayers: [],
-      },
-      availableRooms: [],
+    persist(
+      (set) => ({
+        ...initialRoomsState,
 
-      // Setters
-      setMyRoom: (roomId, gameName, roomPlayers) =>
-        set({ myRoom: { roomId, gameName, roomPlayers } }),
+        // Setters
+        setMyRoom: (roomId, gameName, roomPlayers) =>
+          set({ myRoom: { roomId, gameName, roomPlayers } }),
 
-      setRoomId: (roomId) =>
-        set((state) => ({
-          myRoom: { ...state.myRoom, roomId },
-        })),
+        setRoomId: (roomId) =>
+          set((state) => ({
+            myRoom: { ...state.myRoom, roomId },
+          })),
 
-      setRoomPlayers: (roomPlayers) =>
-        set((state) => ({
-          myRoom: { ...state.myRoom, roomPlayers },
-        })),
+        setRoomPlayers: (roomPlayers) =>
+          set((state) => ({
+            myRoom: { ...state.myRoom, roomPlayers },
+          })),
 
-      setAvailableRooms: (rooms) => set({ availableRooms: rooms }),
+        setAvailableRooms: (rooms) => set({ availableRooms: rooms }),
 
-      // Reset
-      clearRoom: () =>
-        set({ myRoom: { roomId: null, gameName: null, roomPlayers: [] } }),
-    }))
+        // Reset
+        clearRoom: () => set(initialRoomsState, false, "clearRoom"),
+      }),
+      {
+        name: "__rooms",
+      }
+    )
   )
 );
