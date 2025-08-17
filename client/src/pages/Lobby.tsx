2310d517830dae139c7e4ecc9ead7487ba9fd92e
lunoxdev <lunox.code@gmail.com>
2025-08-17 16:32:50 -0600

    temp remove alias

--- client/src/pages/Lobby.tsx
+++ client/src/pages/Lobby.tsx
@@ -1,7 +1,7 @@
-import { useAuthStore } from "@stores/useAuthStore";
-import AuthForm from "@components/auth/AuthForm";
-import LobbyHome from "@components/lobby/LobbyHome";
-import Background from "@components/common/Background";
+import { useAuthStore } from "../stores/useAuthStore";
+import AuthForm from "../components/auth/AuthForm";
+import LobbyHome from "../components/lobby/LobbyHome";
+import Background from "../components/common/Background";
 
 const Lobby = () => {
   const loggedIn = useAuthStore((state) => state.loggedIn);
